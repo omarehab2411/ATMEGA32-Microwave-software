38b97d2bc5262858b4c0e17d5b0c59baadf7f5e2
omar ehab <omarehab45@gmail.com>
2019-10-04 11:58:52 +0200

    Drivers for all hardware used in the project

---
+++ HAL/RTC.h
@@ -0,0 +1,34 @@
+/*
+ * RTC.h
+ *
+ *  Created on: Jan 21, 2019
+ *      Author: omar ehab
+ */
+
+#include <avr/io.h>
+#include "TWI.h"
+#include <util/delay.h>
+
+struct RTC{
+	char sec;
+	char min;
+	char hour;
+	char date;
+	char day;
+	char month;
+	char year;
+};
+
+void RTC_INIT(void);
+void RTC_SET(struct RTC *ptr);
+void  RTC_GET(struct RTC *ptr2);
+//struct RTC *RTC_STRUCT_FILL(int sec,int min,int hour,int date,int day,int month,int year);
+int DEC2BCD(int num);
+int BCD2DEC(int BCD);
+
+#ifndef RTC_H_
+#define RTC_H_
+
+
+
+#endif /* RTC_H_ */
